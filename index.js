f4f3821f51a85e725d215199531dcba966a49d59
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-18 16:30:16 +0600

    first API using express

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const phones = require("./phones.json");
 
 const app = express();
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-   res.send("Welcome to my mobile server");
+   res.send("Welcome to my mobile server. wait till all phone loads");
+});
+
+app.get("/phones", (req, res) => {
+   res.send(phones);
+});
+
+app.get("/phone/:id", (req, res) => {
+   const id = parseInt(req.params.id);
+
+   const phone = phones.find((phone) => phone.id === id) || {};
+
+   res.send(phone);
 });
 
 app.listen(port, () => {
